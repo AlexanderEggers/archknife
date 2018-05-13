39572c1c5f7b1209f1cd3a58b713e691d11f6a49
Mordag <alexander.eggers91@gmail.com>
2018-05-13 15:54:03 +1000

    Fixed build error for providing fragments to dagger

--- processor/src/main/java/archknife/annotation/ProvideActivityProcessor.kt
+++ processor/src/main/java/archknife/annotation/ProvideActivityProcessor.kt
@@ -10,13 +10,11 @@ import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.element.ElementKind
 import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
-import javax.lang.model.type.DeclaredType
 import javax.tools.Diagnostic
 
 class ProvideActivityProcessor : AnnotationProcessor {
 
     private val activitiesWithPackage: HashMap<String, String> = HashMap()
-    private val activitiesHasFragments: HashMap<String, Boolean> = HashMap()
 
     override fun process(mainProcessor: MainProcessor, roundEnv: RoundEnvironment) {
         val fileBuilder = TypeSpec.classBuilder("Generated_ActivityBuilderModule")
@@ -32,6 +30,7 @@ class ProvideActivityProcessor : AnnotationProcessor {
                 .writeTo(mainProcessor.filer)
     }
 
+    @Suppress("LABEL_NAME_CLASH")
     private fun prepareActivityPackageMap(mainProcessor: MainProcessor, roundEnv: RoundEnvironment) {
         roundEnv.getElementsAnnotatedWith(ProvideActivity::class.java)
                 .forEach {
@@ -41,13 +40,6 @@ class ProvideActivityProcessor : AnnotationProcessor {
                     }
 
                     val typeElement = it as TypeElement
-                    typeElement.interfaces.forEach {
-                        val dc = it as DeclaredType
-                        if (dc.asElement().simpleName.toString() == "HasSupportFragmentInjector") {
-                            activitiesHasFragments[typeElement.simpleName.toString()] = true
-                        }
-                    }
-
                     activitiesWithPackage[typeElement.simpleName.toString()] =
                             mainProcessor.elements!!.getPackageOf(typeElement).qualifiedName.toString()
                 }
@@ -56,23 +48,19 @@ class ProvideActivityProcessor : AnnotationProcessor {
     private fun generateActivityProviderMethods(fileBuilder: TypeSpec.Builder, mainProcessor: MainProcessor) {
         activitiesWithPackage.forEach { activityName, packageName ->
             val activityClass = ClassName.get(packageName, activityName)
-            val activityHasFragment: Boolean = activitiesHasFragments[activityName] ?: false
-            val annotationBuilder = AnnotationSpec.builder(classContributesAndroidInjector)
+            val fragmentModuleName = mainProcessor.fragmentModuleMap!![activityName]
 
-            if (activityHasFragment) {
-                val fragmentModuleName = mainProcessor.fragmentModuleMap!![activityName]
-                val classFragmentModule = if (fragmentModuleName != null) {
-                    ClassName.get(MainProcessor.libraryPackage + ".fragment", fragmentModuleName)
-                } else {
-                    classEmptyFragmentModule
-                }
-
-                annotationBuilder.addMember("modules", "$classFragmentModule.class")
+            val classFragmentModule = if (fragmentModuleName != null) {
+                ClassName.get(MainProcessor.libraryPackage + ".fragment", fragmentModuleName)
+            } else {
+                classEmptyFragmentModule
             }
 
             fileBuilder.addMethod(MethodSpec.methodBuilder("contribute$activityName")
                     .addModifiers(Modifier.ABSTRACT)
-                    .addAnnotation(annotationBuilder.build())
+                    .addAnnotation(AnnotationSpec.builder(classContributesAndroidInjector)
+                            .addMember("modules", "$classFragmentModule.class")
+                            .build())
                     .returns(activityClass)
                     .build())
         }
