72f7ec7aa82122ecc0b31d255981aaa68b2cbeed
Mordag <alexander.eggers91@gmail.com>
2020-05-03 03:28:40 +1000

    Tweaking example project

--- examples/src/main/java/org/demo/archknife/MainFragment.kt
+++ examples/src/main/java/org/demo/archknife/MainFragment.kt
@@ -1,5 +1,23 @@
 package org.demo.archknife
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import archknife.annotation.ProvideFragment
+import javax.inject.Inject
 
-class MainFragment : Fragment()
+@ProvideFragment(activityClasses = [MainActivity::class])
+class MainFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var mainViewViewModel: MainActivityViewModel
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mainViewViewModel = ViewModelProvider(viewModelStore, viewModelFactory)
+                .get(MainActivityViewModel::class.java)
+    }
+}
